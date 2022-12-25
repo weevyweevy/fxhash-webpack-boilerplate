8ebde427027d2119f7772e267f16c9767ff4c9dc
Geneva Lee Robles <genevaleerobles@gmail.com>
2022-12-24 22:32:51 -0500

    First Commit - Sin Wave

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,18 @@
+const num = 100;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  stroke(255);
+
+  beginShape();
+  for (let i = 0; i < num; i++){
+    vertex(i*width/num, height / 2+50*sin(i));
+    
+  }
+ 
+  endShape();
 }
